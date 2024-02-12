28dff510fbc3bd52e6b128b14e36033b12d41626
Melleo <148089993+d-melleo@users.noreply.github.com>
2024-02-12 12:36:44 +0200

    Update mongo.py

--- utils/mongo.py
+++ utils/mongo.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-USERNAME = 'admin'
-PASSWORD = 'hmcblUZX6d5w5V6p'
+USERNAME = 'your_username'
+PASSWORD = 'your_password'
 CONNECTION_STRING = f'mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.qh8t139.mongodb.net/'
 CLIENT = MongoClient(CONNECTION_STRING)
